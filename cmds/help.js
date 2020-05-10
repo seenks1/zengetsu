78f8083d837d491a4717ba3a620cbd9208090225
Glitch (starter-discord) <none>
2020-05-10 22:41:12 +0000

    👽💍 Checkpoint ./cmds/help.js:9816165/230

--- cmds/help.js
+++ cmds/help.js
@@ -1,7 +1,9 @@
 const Discord = require('discord.js')
 
 module.exports.run = async (client, message, args, ops) => {
-  
+  let embed = new Discord.RichEmbed()
+    .setTitle('**Commands**')
+    .addField('Latency:', 'Shows the current ping between you and the server')
 }
 
 module.exports.help = {
