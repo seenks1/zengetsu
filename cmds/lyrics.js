92f1ba7792a47dbe7152b72464d84b97a2e99233
Glitch (starter-discord) <none>
2020-05-10 15:37:55 +0000

    🚗🌉 Checkpoint ./cmds/lyrics.js:9816165/575 ./package.json:9816165/1694

---
+++ cmds/lyrics.js
@@ -0,0 +1,18 @@
+const solenolyrics= require("solenolyrics");
+const Discord = require('discord.js');
+
+module.exports.run = async (client, message, args, ops) => {
+  let fetched = ops.active.get(message.guild.id);
+  if(!fetched) return message.channel.send('There currently isn\'t any music playing in this guild!');
+	
+	let queue = fetched.queue
+	let nowPlaying = queue[0];
+	var lyrics = await solenolyrics.requestLyricsFor(queue[0]);
+	
+	message.channel.send(lyrics)
+	
+}
+
+module.exports.help = {
+	name: "lyrics"
+}
