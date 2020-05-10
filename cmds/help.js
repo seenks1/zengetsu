bce4c78891dc29bcd3b7fefdbbb2c7ba1e33cd01
Glitch (starter-discord) <none>
2020-05-10 22:53:15 +0000

    🐠🚘 Checkpoint ./cmds/help.js:9816165/827

--- cmds/help.js
+++ cmds/help.js
@@ -4,6 +4,18 @@ module.exports.run = async (client, message, args, ops) => {
   let embed = new Discord.RichEmbed()
     .setTitle('**Commands**')
     .addField('Latency:', 'Shows the current ping between you and the server')
+    .addField('Loop', 'Loops the current queue of music in the guild')
+    .addField('Lyrics', 'Display the lyrics of the song currently playing in the guild')
+    .addField('Pause', 'Pauses the current music being played in the guild')
+    .addField('Play', 'Play music in the guild. Currently supports **Youtube** and **Spotify**')
+    .addField('Queue', 'Displays the current working music queue')
+    .addField('Resume', 'Resumes the music in the guild')
+    .addField('Skip', 'Vote skips the current song playing in the guild')
+    .addField('Volume', 'Changes the volume of the song currently playing in the guild')
+    .setFooter('Showing the list of currently available cmds')
+  
+  message.channel.send(embed)
+  
 }
 
 module.exports.help = {
