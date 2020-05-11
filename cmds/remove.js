34d56c594b4bc6aa810537dd8d363fc7fb5162f0
Glitch (starter-discord) <none>
2020-05-11 01:02:52 +0000

    ⚽️🍭 Checkpoint ./cmds/remove.js:9816165/306

--- cmds/remove.js
+++ cmds/remove.js
@@ -10,7 +10,9 @@ module.exports.run = async (client, message, args, ops) => {
   let nowPlaying = queue[0];
   
   if (!args[0]) return message.channel.send('You must specify which song you wish to remove!')
-  if (args[0].isNaN() || args[0] > queue.length) return message.channel.send('That\'t not a valid queue position!')
+  if (isNaN(args[0]) || args[0] > queue.length) return message.channel.send('That\'t not a valid queue position!')
+  let index = args[0]
+  message.channel.send(`Successful removed **${queue[index].songTitle}** from the queue!`)
   queue.splice(args[0], 1);
 }
 
