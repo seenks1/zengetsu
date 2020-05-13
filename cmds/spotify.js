7641c0bc4ec0fd7d229b6d71b3077151b14666b8
Glitch (starter-discord) <none>
2020-05-13 16:15:22 +0000

    🐌✌️ Checkpoint ./cmds/spotify.js:9816165/2881

--- cmds/spotify.js
+++ cmds/spotify.js
@@ -3,34 +3,34 @@ const Discord = require('discord.js');
 module.exports.run = (client, message, args) => {
 	
 	let user = message.mentions.users.first() || message.author;
-	
-	if (user.presence.activity !== null && user.presence.activity.type === 'LISTENING' && user.presence.activity.name === 'Spotify' && user.presence.activity.assets !== null) {
+	user.presence.activities.forEach((activity) => {
+    if (activity !== null && activity.type === 'LISTENING' && activity.name=== 'Spotify' && activity.assets !== null) {
 		
-		let trackIMG = `https:/i.scdn.co/image/${user.presence.game.assets.largeImage.slice(8)}.png`;
-		
-		let trackURL = `https://open.spotify.com/track/${user.presence.game.syncID}`;
-		
-		let trackName = user.presence.game.details;
-		let trackAuthor = user.presence.game.state;
-		let trackAlbum = user.presence.game.assets.largeText;
-		
-		const embed = new Discord.MessageEmbed()
-		.setAuthor('Spotify Track Info', user.avatarURL())
-		.setColor(0x1ED760)
-		//.setThumbnail(user.avatarURL)
-		.setThumbnail(user.presence.game.assets.largeImageURL())
-		.addField('Song Name', trackName, true)
-		.addField('Album', trackAlbum, true)
-		.addField('Author', trackAuthor, false)
-		.addField('Link to this track: ', `[\`${trackURL}\`](trackURL)`, false)
-		
-		message.channel.send(embed);
-		
-	} else {
-		
-		message.channel.send('**This user isn\'t currently listening to anything on Spotify!**');
-	}
-	
+      let trackIMG = `https:/i.scdn.co/image/${user.presence.activities.assets.largeImage.slice(8)}.png`;
+
+      let trackURL = `https://open.spotify.com/track/${user.presence.activities.syncID}`;
+
+      let trackName = user.presence.activities.details;
+      let trackAuthor = user.presence.activities.state;
+      let trackAlbum = user.presence.activities.assets.largeText;
+
+      const embed = new Discord.MessageEmbed()
+      .setAuthor('Spotify Track Info', user.avatarURL())
+      .setColor(0x1ED760)
+      //.setThumbnail(user.avatarURL)
+      .setThumbnail(user.presence.activities.assets.largeImageURL())
+      .addField('Song Name', trackName, true)
+      .addField('Album', trackAlbum, true)
+      .addField('Author', trackAuthor, false)
+      .addField('Link to this track: ', `[\`${trackURL}\`](trackURL)`, false)
+
+      message.channel.send(embed);
+
+    } else {
+      message.channel.send('**This user isn\'t currently listening to anything on Spotify!**');
+    }
+    
+  }
 	
 	
 }
