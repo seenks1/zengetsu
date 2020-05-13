14875593d2f28149ff711d00873b5c8dfb7a4099
Glitch (starter-discord) <none>
2020-05-13 16:27:21 +0000

    🍔🌊 Checkpoint ./cmds/spotify.js:9816165/949

--- cmds/spotify.js
+++ cmds/spotify.js
@@ -3,22 +3,23 @@ const Discord = require('discord.js');
 module.exports.run = (client, message, args) => {
 	
 	let user = message.mentions.users.first() || message.author;
+  
 	user.presence.activities.forEach((activity) => {
-    if (activity !== null && activity.type === 'LISTENING' && activity.name=== 'Spotify' && activity.assets !== null) {
+    if (activity.type === 'LISTENING' && activity.name === 'Spotify' && activity.assets !== null) {
 		
-      let trackIMG = `https:/i.scdn.co/image/${user.presence.activities.assets.largeImage.slice(8)}.png`;
+      let trackIMG = `https:/i.scdn.co/image/${activity.assets.largeImage.slice(8)}.png`;
 
-      let trackURL = `https://open.spotify.com/track/${user.presence.activities.syncID}`;
+      let trackURL = `https://open.spotify.com/track/${activity.syncID}`;
 
-      let trackName = user.presence.activities.details;
-      let trackAuthor = user.presence.activities.state;
-      let trackAlbum = user.presence.activities.assets.largeText;
+      let trackName = activity.details;
+      let trackAuthor = activity.state;
+      let trackAlbum = activity.assets.largeText;
 
       const embed = new Discord.MessageEmbed()
       .setAuthor('Spotify Track Info', user.avatarURL())
       .setColor(0x1ED760)
       //.setThumbnail(user.avatarURL)
-      .setThumbnail(user.presence.activities.assets.largeImageURL())
+      .setThumbnail(activity.assets.largeImageURL())
       .addField('Song Name', trackName, true)
       .addField('Album', trackAlbum, true)
       .addField('Author', trackAuthor, false)
@@ -30,7 +31,7 @@ module.exports.run = (client, message, args) => {
       message.channel.send('**This user isn\'t currently listening to anything on Spotify!**');
     }
     
-  }
+  })
 	
 	
 }
