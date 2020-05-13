47a1b2a011f67fbf006cf27ca3b2b3fa81f7576a
Glitch (starter-discord) <none>
2020-05-13 01:43:42 +0000

    🦂🐝 Checkpoint ./cmds/lyrics.js:9816165/376

--- cmds/lyrics.js
+++ cmds/lyrics.js
@@ -2,6 +2,7 @@ const solenolyrics = require("solenolyrics");
 const Discord = require("discord.js");
 
 module.exports.run = async (client, message, args, ops) => {
+  let trimmed = ''
   let fetched = ops.active.get(message.guild.id);
   if (!fetched)
     return message.channel.send(
@@ -10,11 +11,17 @@ module.exports.run = async (client, message, args, ops) => {
 
   let queue = fetched.queue;
   let nowPlaying = queue[0];
-  var lyrics = await solenolyrics.requestLyricsFor(queue[0]);
-  for (let i = 0; i < string.length; i++){
-  console.log(string[i])
-}
-  message.channel.send(lyrics.trim());
+  var lyrics = await solenolyrics.requestLyricsFor(queue[0].songTitle);
+  for (let i = 0; i < 1500; i++){
+    trimmed += lyrics[i]
+  }
+  
+  let embed = new Discord.MessageEmbed()
+    .setColor(0xFFFF00)
+    .setTitle('**Lyrics**')
+    .setDescription(trimmed)
+  
+  message.channel.send(embed);
 };
 
 module.exports.help = {
