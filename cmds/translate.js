3cd9d975c40eccac6561595accda585573b16667
Glitch (starter-discord) <none>
2020-05-24 22:48:54 +0000

    👣⚡️ Checkpoint ./cmds/translate.js:9816165/244

--- cmds/translate.js
+++ cmds/translate.js
@@ -1,5 +1,10 @@
+const translate = require('translate');
+translate.key = 'AIzaSyCwGh6sW0oPGsMwvWroAPssXPwm33L_zRw'
+
 module.exports.run = async function(client, message, args, ops) {
   if (!args[0]) return message.channel.send('Give me some text to translate!')
+  const foo = await translate(args[0], 'es');
+  message.channel.send(`Translated Message: **${foo}**`)
 }
 
 module.exports.help = {
