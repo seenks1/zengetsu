37e9894cf46de2316b710a3a67855482a20d39b4
Glitch (starter-discord) <none>
2020-05-24 23:24:54 +0000

    ⏳😜 Checkpoint ./cmds/dskip.js:9816165/2

--- cmds/dskip.js
+++ cmds/dskip.js
@@ -2,7 +2,7 @@ var skip = false;
 
 module.exports.run = async function(client, message, args, ops) {
  	let voiceChannel = message.member.voiceChannel
-	//if (!message.member.voice.channel) return message.channel.send('You are not currently connected to a voice channel!')
+	if (!message.member.voice.channel) return message.channel.send('You are not currently connected to a voice channel!')
   if (!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('Only administrators have access to this command')
 	if (skip === false) {
 		skip = true;
