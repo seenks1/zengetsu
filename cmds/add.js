19ff6287095053825796a7d27cb8d65cc7f78e07
Glitch (starter-discord) <none>
2020-05-25 19:44:45 +0000

    🍗🐜 Checkpoint ./cmds/add.js:9816165/334

--- cmds/add.js
+++ cmds/add.js
@@ -10,16 +10,18 @@ module.exports.run = async function (client, message, args, ops) {
 	if(!fetched) return message.channel.send('There currently isn\'t any music playing in the guild!');  
   
   keyv.on('error', err => console.error('Keyv connection error:', err));
-  if (!await keyv.get('favorite1' + message.author.id)) {
+  if (!await keyv.get(`favorite1${message.author.id}`)) {
+    console.log('This works!')
     for (var i = 1; i < 5; i++) {
-      await keyv.set(`favorite${i}` + message.author.id, '')
+      await keyv.set(`favorite${i}${message.author.id}`, '')
     }
   }
 
-  for (var i = 1; i < 5; i++) {
-    let favorites = await keyv.get(`favorite{i}` + message.author.id)
+  for (var i = 0; i < 5; i++) {
+    let favorites = await keyv.get(`favorite${i}${message.author.id}`)
+    console.log(favorites)
     if (favorites.includes(fetched.queue[0].songTitle)) return message.channel.send('❌ This song is already favorited!')
-    keyv.set(message.author.id, `${fetched.queue[0].songTitle} [[Click Here]](${fetched.queue[0].url})`)
+    keyv.set(`favorite${i}${message.author.id}`, `${fetched.queue[0].songTitle} [[Click Here]](${fetched.queue[0].url})`)
   }
   message.channel.send(`⭐ Successfully added **${fetched.queue[0].songTitle}** to your favorites! ⭐`)
   
