d55602943eeb5810e88b58c055e2a4ecc4dd7967
Glitch (zengetsu) <none>
2020-05-28 15:05:24 +0000

    👨‍👨‍👦🍓 Checkpoint ./cmds/vote.js:9816165/431

---
+++ cmds/vote.js
@@ -0,0 +1,8 @@
+module.exports.run = async function (client, message, args, ops) {
+  
+  message.channel.send(`[Vote for Zengetsu on Discord Bot List!](https://top.gg/bot/645779244177162240)`)
+}
+
+module.exports.help = {
+  name: 'vote'
+}
