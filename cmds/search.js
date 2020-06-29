cc571e7f3e8dcee3bd79b92eb75584f96e9e5354
KinetixGaming <lawr3572@gmail.com>
2020-06-29 10:06:29 -0400

    Fixed

--- cmds/search.js
+++ cmds/search.js
@@ -3,37 +3,34 @@ const Discord = require('discord.js')
 
 module.exports.run = (client, message, args, ops) => {
 	let mid = ''
-  
-  const server = client.guilds.cache.get('666058424731041801')
-  const load = server.emojis.cache.last()
-  
+
 	search(args.join(' '), function(err, res) {
 		if (err) return console.log(err)
-		
-		let videos = res.videos.slice(0,10); 
-		
+
+		let videos = res.videos.slice(0,10);
+
 		let resp = '';
 		for (var i in videos) {
 			//resp += `**[${parseInt(i)+1}]:** \`${videos[i].title} (${videos[i].duration.timestamp})\`\n`;
 			resp += `**[${parseInt(i)+1}]:** ${videos[i].title} **(${videos[i].duration.timestamp})**\n\n`;
 		}
-		
+
 		resp += `\n**Choose a number between** \`1-${videos.length}\``;
-		
+
 		let embed = new Discord.MessageEmbed()
 			.setColor(0xFFFF00)
 			.setTitle('Results')
 			.setDescription(resp)
-	
-		message.channel.send(` ${load} Loading...`).then((sentMessage) => sentMessage.edit(embed).then(msg => mid = msg))
-		
+
+		message.channel.send(`Loading...`).then((sentMessage) => sentMessage.edit(embed).then(msg => mid = msg))
+
 		const filter = m => !isNaN(m.content) && m.content < videos.length + 1 && m.content > 0 && m.author.id === message.author.id;
-		
+
 		const collector = message.channel.createMessageCollector(filter);
-		
-		
+
+
 		collector.videos = videos;
-    
+
 		collector.once('collect', function(m) {
       mid.delete()
 			let commandFile = require(`./play.js`);
