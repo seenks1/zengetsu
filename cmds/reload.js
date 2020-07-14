f84c4439a1ad05a1e0ff67f85cdeb75eb2094616
KinetixGaming <lawr3572@gmail.com>
2020-07-14 02:43:20 -0400

    Even more fixing, I love my life!

--- cmds/reload.js
+++ cmds/reload.js
@@ -1,5 +1,5 @@
 module.exports.run = async function(client, message, args, ops) {
-  if (!args) return message.channel.send(`You didn't pass any command to reload, ${message.author}!`);
+  if (!args[0]) return message.channel.send(`You didn't pass any command to reload, ${message.author}!`);
   const commandName = args[0].toLowerCase();
   const command = message.client.commands.get(commandName) || message.client.commands.find(cmd => cmd.help.aliases && cmd.help.aliases.includes(commandName));
 
@@ -8,14 +8,14 @@ module.exports.run = async function(client, message, args, ops) {
 
   try {
 
-  	const newCommand = require(`./${command.name}.js`);
+  	const newCommand = require(`./${command.help.name}.js`);
   	client.commands.set(newCommand.name, newCommand);
-    return message.channel.send(`Command \`${command.name}\` was reloaded!`);
+    return message.channel.send(`Command \`${command.help.name}\` was reloaded!`);
 
   } catch (error) {
 
   	console.log(error);
-  	message.channel.send(`There was an error while reloading a command \`${command.name}\`:\n\`${error.message}\``);
+  	message.channel.send(`There was an error while reloading a command \`${command.help.name}\`:\n\`${error.message}\``);
 
   }
 }
