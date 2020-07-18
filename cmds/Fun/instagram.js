9b29a50517db70233c768ae4d226b2f06f3bc20a
KinetixGaming <lawr3572@gmail.com>
2020-07-18 02:19:10 -0400

    Added Urban Dictionary and Instagram Lookup Commands.

---
+++ cmds/Fun/instagram.js
@@ -0,0 +1,35 @@
+const Discord = require("discord.js")
+const stripIndents = require("common-tags");
+const fetch = require("node-fetch");
+
+module.exports.run = async function (client, message, args, ops) {
+  const name = args.join(' ')
+  if (!name) return message.reply('Please supply an instagram username').then(m => m.delete(5000));
+
+  const url = `https://instagram.com/${name}/?__a=1`;
+  const res = fetch(url => url.json());
+
+  if (!res.graphql.user.username) return message.channel.send('I couldn\'t find that user')
+
+  const account = res.graphql.user;
+
+  const embed = new Discord.MessageEmbed()
+    .setColor('RANDOM')
+    .setTitle(account.fullname)
+    .setURL(account.external_url_linkshimmed)
+    .addField("Profile Information", stripIndents`**- Username:** ${account.username}
+    **- Full name:** ${account.full_name}
+    **- Biography; ** ${account.biography.length == 0 ? "none" : account.biography}
+    **- Posts:** ${user.edge_owner_to_timeline_media}
+    **- Followers:** ${account.edge_follow.count}
+    **- Following:** ${account.edge_follow.count}
+    **- Private Account:** ${account.is_private} ? "Yes" : Nope"`);
+
+  message.channel.send(embed)
+
+}
+
+module.exports.help = {
+  name: 'instagram',
+  aliases: ['insta', 'in']
+}
---
+++ cmds/Fun/urban.js
@@ -0,0 +1,23 @@
+const urban = require('relevant-urban')
+const Discord = require('discord.js')
+
+module.exports.run = async function (client, message, args, ops) => {
+  if (!args[0]) return message.channel.send('Specify some text to search for.')
+  let res = await urban(args.join(' ')).catch(e => {
+    return message.channel.send('That word could not be found.')
+  })
+
+  const embed = new Discord.MessageEmbed()
+    .setColor('RANDOM')
+    .setTitle(res.word)
+    .setURL(res.urbanURL)
+    .setDescription(`**Definition:**\n*${res.definition}*\n\n**Example:**\n*${res.example}*`)
+    .addField('Author ', res.author, true)
+    .addField('Rating', `**\`Upvotes: ${res.thumbsUp} | Downvotes: ${res.thumbsDown}\`**`)
+
+    if (res.tags.length > 0 && res.tags.join(', ').length < 2074) {
+      embed.addField('Tags', res.tags.join(', '), true)
+    }
+
+    message.channel.send(embed);
+}
