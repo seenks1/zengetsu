fec437b8b45e270b2a49610384edf0124e7ea560
KinetixGaming <lawr3572@gmail.com>
2020-07-19 17:13:20 -0400

    Showdown fixed

--- cmds/Fun/showdown.js
+++ cmds/Fun/showdown.js
@@ -3,27 +3,33 @@ const Discord = require('discord.js')
 module.exports.run = async function (client, message, args, ops) {
   if (!args[0]) return message.channel.send('Please mention a user you would like to duel!')
   if (!message.mentions.users.first()) return message.channel.send('That\'s not a valid mention.')
+  if(message.mentions.users.first() === message.author) return message.channel.send('You cannot duel yourself!')
+  if (message.mentions.users.first().bot) return message.channel.send('You can only duel humans. Idiot.')
   let user2 = message.mentions.users.first().id
   let user1 = message.author.id
   message.channel.send(`${message.mentions.users.first()}, you\'ve been challenged to a duel by ${message.author}, do you accept?`)
-  const collector = new Discord.MessageCollector(message.channel, m => m.author.id === message.mentions.users.first().id);
+  const collector = new Discord.MessageCollector(message.channel, m => m.author.id === message.mentions.users.first().id, {idle: 10000});
   collector.on('collect', message => {
     if (message.content.toLowerCase().includes('yes') || message.content.toLowerCase().includes('yup')) {
       showdown(client, message, args, user1, user2)
-    } else if (message.content.toLowerCase().includes('yes') || message.content.toLowerCase().includes('yup')) {
-        return message.channel.send('Declined!')
+    } else if (message.content.toLowerCase().includes('no') || message.content.toLowerCase().includes('nope')) {
+        message.channel.send('Declined!')
       }
 
-    collector.stop()
+    collector.stop('Finished')
+  })
+
+  collector.on('end', (collected, reason) => {
+    if (reason !== 'Finished') return message.channel.send('You took too long to respond...')
   })
 
   async function showdown (client, message, args, user1, user2) {
     message.channel.send('First one to type `shoot` when the clock strikes wins!')
+    await new Promise(done => setTimeout(done, Math.random() * (5000 - 500) + 500));
     message.channel.send('Ding!');
-    await new Promise(done => setTimeout(done, 3000));
-    let scollector = new Discord.MessageCollector(message.channel, m => m.author.id == user1 || m.author.id == user2);
+    let scollector = new Discord.MessageCollector(message.channel, m => m.author.id == user1 || m.author.id == user2 && m.content.toLowerCase().includes('shoot'));
     scollector.on('collect', message => {
-      if (message.content.includes('shoot')) message.channel.send(`${message.author} has won the showdown!`)
+      message.channel.send(`${message.author} has won the showdown!`)
       return scollector.stop()
     })
   }
