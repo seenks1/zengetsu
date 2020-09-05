0f19a7b09ff5ca52423ad28c9a7af40eb1a73af0
KinetixGaming <lawr3572@gmail.com>
2020-09-05 18:51:29 -0400

    Fixed

--- bot.js
+++ bot.js
@@ -50,10 +50,9 @@ client.on("ready",  async () => {
 });
 
  client.on('messageDelete', message => {
-  //console.log(`${message.id} was deleted!`);
-  // Partial messages do not contain any content so skip them
+  console.log(`${message.id} was deleted!`);
   if (!message.partial) {
-    //console.log(`Someone deleted a message! It had content: **"${message.content}"**`);
+    console.log(`Someone deleted a message! It had content: **"${message.content}"**`);
   }
 });
 
